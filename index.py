e21452a9f6b3a86583491947fbe91c248c7fcb35
naresh0101 <naresh18@navgurukul.org>
2021-08-10 13:41:36 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,26 @@
+
+from PIL import Image
+import PIL
+import glob
+from pathlib import Path
+import os
+
+def compressFile(basePath):
+    fixed_height = 200
+    savingFolder = basePath.split('/')[1]
+    if not os.path.exists('afterCompress/'+savingFolder):
+        os.makedirs('afterCompress/'+savingFolder)
+    savingpath = 'afterCompress'+basePath+'.webp'
+    filepath = 'fileSToCompress'+basePath+'.png'
+    image = Image.open(filepath)
+    height_percent = (fixed_height / float(image.size[1]))
+    width_size = int((float(image.size[0]) * float(height_percent)))
+    image = image.resize((width_size, fixed_height), PIL.Image.NEAREST)
+    image.save(savingpath,quality=80)
+
+FilesToCompress = Path('fileSToCompress').glob('**/*.png')
+for path in FilesToCompress:
+    basePath = str(path).split('.png')[0].split('fileSToCompress')[1]
+    compressFile(basePath)
+    
+# doc install pip3 pillow install 
